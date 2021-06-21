f3bd7615186c0c9526a4abb25b5c630819786624
Mac <mac@MacdeMacBook-Pro.local>
2021-06-21 15:05:36 +0800

    RouterIndex.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from "../views/loginView/LoginView";
 
 const routes = [
+  {
+    path:'/',
+    redirect:'/login',
+  },
+  {
+    path:'/login',
+    component:LoginView,
+  },
+  {
+    path: '/user',
+    component: () => import('views/userView/UserView'),
+    beforeEnter: (to, from, next) => {
+      if(from.path === '/login'){
+        next();
+      }else{
+        alert('您还没有登录，请先登录');
+        next('/login');
+      }
+    }
+  },
+  {
+    path:'/admin',
+    component: () => import('views/adminView/AdminView'),
+    beforeEnter:(to,from,next) => {
+      if(from.path === '/login'){
+        next()
+      }else{
+        alert('您还没有登录，请先登录');
+        next('/login')
+      }
+  }
+  }
 ]
 
 const router = createRouter({
