166026d35707b6272c8f0318c29a297b3f326caa
Mac <mac@MacdeMacBook-Pro.local>
2021-06-21 15:07:23 +0800

    VuexIndex.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
 import { createStore } from 'vuex'
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 export default createStore({
   state: {
+    //居民用户信息
+    UserAccountNumber:[
+      {
+        user:'user',
+        email:'user1',
+        userpassword:'123456'
+      },
+    ],
+    AdminAccountNumber:[
+      {
+        admin:'admin',
+        email:'admin1',
+        adminpassword:'654321'
+      }
+    ]
   },
-  mutations: {
+  modules:{
+
   },
-  actions: {
-  },
-  modules: {
-  }
+  getters,
+  mutations,
+  actions,
+
+
 })
