ec9a8032d05b8bce9c392ae99ad647cf51ffae0d
Mac <mac@MacdeMacBook-Pro.local>
2021-06-23 01:43:47 +0800

    UserBodyLeftNavBar-Assembly

---
+++ src/components/content/userViewSubassembly/UserBody/UserBodyLeftNavBar.vue
@@ -0,0 +1,21 @@
+<template>        <!--Body左边导航栏-->
+  <div class="nav-bar-box">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserBodyLeftNavBar"
+}
+</script>
+
+<style scoped>
+.nav-bar-box{
+  width: 80%;
+  height: 100%;
+  margin: auto;
+}
+</style>
