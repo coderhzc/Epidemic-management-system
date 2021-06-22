79482beb844a50dcd94ea8e147a9bf4f1fdb3cd3
Mac <mac@MacdeMacBook-Pro.local>
2021-06-23 01:40:20 +0800

    UserNavBarLocation-SubAssembly

---
+++ src/components/content/userViewSubassembly/UserNavbar/UserNavBarLocation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="nav-bar-location">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+
+    <div class="right">
+    <slot name="right"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserNavBarLocation"
+}
+</script>
+
+<style scoped>
+.nav-bar-location{
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: #6fb0ff;
+}
+.left{
+  position: relative;
+  width: 13%;
+  background-color: #55a1ff;
+  box-shadow: 2px 0 4px rgba(40, 82, 132, 0.71);
+}
+.right{
+  position: relative;
+  width: 15%;
+  background-color: #55a1ff;
+  box-shadow: -2px 0 4px rgba(40, 82, 132, 0.71);
+}
+.center{
+  position: relative;
+  flex: 1;
+}
+</style>
