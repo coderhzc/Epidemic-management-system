53549a5b2186a269dad28f7ea77f76dd9ecac57e
Mac <mac@MacdeMacBook-Pro.local>
2021-06-23 01:43:05 +0800

    UserBodyLocation-SubAssembly

---
+++ src/components/content/userViewSubassembly/UserBody/UserBodyLocation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="body-location">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserBodyLocation"
+}
+</script>
+
+<style scoped>
+.body-location{
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+.left{
+  width: 13%;
+  background-color: #55a1ff;
+}
+.right{
+  flex: 1;
+  background-color: orange;
+}
+</style>
